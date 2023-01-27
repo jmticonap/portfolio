d0f2717be741da6bed08c87fb6f897f6967289f0
juancho <jm.ticona.p@gmail.com>
2023-01-27 07:11:51 -0500

    FIX Hearder.css responsive nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,22 +6,22 @@ import IconGitHub from '@mui/icons-material/GitHub'
 import IconEmail from '@mui/icons-material/EmailRounded'
 import imgProgrammer from '../assets/img/yo_1_x_1.png'
 
-const Header = ({name, linkedinPath, githubPath, email}) => {
+const Header = ({ name, linkedinPath, githubPath, email }) => {
 
     const themeSocials = {
-        fontSize: '4rem', 
-        color:'var(--hight-light)'
+        fontSize: '4rem',
+        color: 'var(--hight-light)'
     }
 
     return (
         <header className="page-cover">
-            <NavigatorBar />
+            {/* <NavigatorBar /> */}
             <div className="page-cover__title">
                 <h1 className="page-cover__title__h1">Juan Ticona</h1>
                 <div className="page-cover__title__div">
                     <p>
-                        I am a full stack developer, 
-                        with a continius effots to get new skills and knowledge an I have 
+                        I am a full stack developer,
+                        with a continius effots to get new skills and knowledge an I have
                         <span className="highlight"> +1 year of experience.</span><br />
                     </p>
                     <div className="page-cover__socials">
@@ -32,18 +32,18 @@ const Header = ({name, linkedinPath, githubPath, email}) => {
                         </div>
                         <div>
                             <a href={githubPath} target="_blank" >
-                                <IconGitHub sx={themeSocials} />                            
+                                <IconGitHub sx={themeSocials} />
                             </a>
                         </div>
                         <div>
                             <a href={`mailto:${email}`}>
                                 <IconEmail sx={themeSocials} />
-                            </a>                            
+                            </a>
                         </div>
                     </div>
                 </div>
-                <img 
-                    className="page-cover__title__img" 
+                <img
+                    className="page-cover__title__img"
                     src={imgProgrammer} alt={`${name} - Photo`}
                     title={name} />
             </div>
